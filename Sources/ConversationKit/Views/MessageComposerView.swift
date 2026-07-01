4d64c29fb1e1567557c99bb65eac05332d30443c
Peter Friese <peter@peterfriese.de>
2026-07-01 11:33:07 +0200

    refactor(preview): update MessageComposerView previews with descriptive prompts
    Replaces generic 'Hello, world!' with context-aware user queries about library features.

--- Sources/ConversationKit/Views/MessageComposerView.swift
+++ Sources/ConversationKit/Views/MessageComposerView.swift
@@ -1,20 +1,21 @@
 //
-// MessageComposerView.swift
-// ConversationKit
+//  MessageComposerView.swift
+//  ConversationKit
 //
-// Created by Peter Friese on 03.07.25.
+//  Created by Peter Friese on 03.07.25.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
 
 import SwiftUI
 
@@ -272,7 +273,7 @@ extension MessageComposerView where AttachmentType == EmptyAttachment {
 }
 
 #Preview("With Attachments") {
-  @Previewable @State var message = "Hello, world!"
+  @Previewable @State var message = "How does the sticky top scroll work?"
   @Previewable @State var attachments = [
     ImageAttachment(image: PlatformImage.systemSymbol("photo")!),
     ImageAttachment(image: PlatformImage.systemSymbol("camera")!),
@@ -289,9 +290,8 @@ extension MessageComposerView where AttachmentType == EmptyAttachment {
 }
 
 #Preview("Without Attachments") {
-    @Previewable @State var message = "Hello, world!"
+    @Previewable @State var message = "Tell me more about ConversationKit."
 
     MessageComposerView(message: $message)
 
 }
-
